3008d4c123311920667a108e1899bb91f6c4c05f
quangbaort <quangbaort@gmail.com>
2023-02-15 22:06:53 +0700

    commit

--- database/conbection.js
+++ database/conbection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         mongoose.set("strictQuery", false);
-        await mongoose.connect('mongodb+srv://captcha1:dCA7Gd1xmOTaCYof@captcha.ind1sb1.mongodb.net/?retryWrites=true&w=majority', () => {
+        await mongoose.connect('mongodb://localhost:27017', () => {
             console.log('MongoDB: connected')
         })
     } catch (error) {
